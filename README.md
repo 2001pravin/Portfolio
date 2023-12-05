b9e8615412e1e76f2fd4143d3a7e0d0d389615ee
pravin pralhad patil <113687095+2001pravin@users.noreply.github.com>
2023-12-05 23:50:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+# Portfolio
+# portfoli-p
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#388af5",
+    text_primary:"#F2F3F4",
+    text_secondary:"#0e0f0f",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#388af5",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
